25e37308f1dd023793f37968d3b2d1b33182be71
restjohn <restjohn@users.noreply.github.com>
2019-03-13 15:38:28 -0600

    break up the command line script and the main tiles task; support env parameters through confabulous

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
+const Confabulous = require('confabulous');
+const wetsaw = require('./wetsaw');
 
 const parseXsltParam = function(pair) {
   if (!pair) {
@@ -28,216 +30,83 @@ const parseBBox = function(arg) {
 const commandLineOptions = [
   { name: 'help', type: Boolean, description: 'display this help message' },
   { name: 'bbox', type: parseBBox, typeLabel: '"<string>"', description: 'bounding box quoted string of space-separated numeric values; "<west> <south> <east> <north>"' },
-  { name: 'zmin', type: parseInt, description: 'XYZ tile map minimum zoom level', typeLabel: '<int>' },
-  { name: 'zmax', type: parseInt, description: 'XYZ tile map maximum zoom level', typeLabel: '<int>' },
+  { name: 'zoom-min', type: parseInt, description: 'XYZ tile map minimum zoom level', typeLabel: '<int>' },
+  { name: 'zoom-max', type: parseInt, description: 'XYZ tile map maximum zoom level', typeLabel: '<int>' },
   { name: 'style', description: 'path to the Mapnik XML style document' },
   { name: 'xslt', description: 'path to an XML Stylesheet Language Transform (XSLT) document to transform the Mapnik XML style document' },
   { name: 'xslt-param', type: parseXsltParam, typeLabel: '"<name>:<value>"', description: 'parameter for style-xslt with name and value separated by a colon (no whitespace); use environment variables by prefixing the parameter value with "env."', multiple: true },
-  { name: 'gpkg', description: 'path to the GeoPackage file to create or update', defaultValue: 'tiles.gpkg' },
+  { name: 'gpkg', description: 'path to the GeoPackage file to create or update; defaults to the basename of the Mapnik style file' },
   { name: 'table', description: 'name of the tile table to create in the GeoPackage; defaults to the basename of the GeoPackage file without the .gpkg extension' },
   { name: 'table-label', description: 'human-readable short name of the tile table; the contents table \'identifier\' column' },
   { name: 'table-desc', description: 'human-readable description of the tile table; the contents table \'description\' column' },
   { name: 'scale', type: parseFloat, typeLabel: '<float>', description: 'scale to apply to the tile images; the output tile size will be the scale * 256', defaultValue: 1.0 }
 ];
 
-const args = commandLineArgs(commandLineOptions, { camelCase: true });
-const { help, bbox, zmin: minZoom, zmax: maxZoom, style, styleXslt: xslt, xsltParam: xsltParamList, gpkg, table, tableLabel, tableDesc, scale } = args;
-
-if (help || !(bbox && Number.isInteger(minZoom) && Number.isInteger(maxZoom))) {
-  console.log(commandLineUsage([{ header: 'Usage:', optionList: commandLineOptions }]));
-  process.exit(0);
-}
-
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const path = require('path');
-
-const MetaTile = require('./meta-tile');
-
-const mapnik = require('mapnik');
-const mapnikPool = require('mapnik-pool')(mapnik);
-const mapTiles = require('global-mercator');
-const gpkgUtil = require('@ngageoint/geopackage');
-const untildify = require('untildify');
-const xsltproc = require('xsltproc');
-
-/**
- * Create a GeoPackage `BoundingBox` object from the given coordinates.
- * @param {number} west
- * @param {number} south
- * @param {number} east
- * @param {number} north
- */
-function gpkgBBoxForSWNE(west, south, east, north) {
-  return new gpkgUtil.BoundingBox(west, east, south, north);
-}
-
-const stylePath = path.resolve(untildify(style));
-const styleDir = path.dirname(stylePath);
-const xsltPath = xslt ? path.resolve(untildify(xslt)) : null;
-const gpkgPath = path.resolve(untildify(gpkg)) + (/\.gpkg$/.test(gpkg) ? '' : '.gpkg');
-const gpkgDir = path.dirname(gpkgPath);
-const tableName = table || path.basename(gpkgPath).slice(0, -5);
-
-const matrixSetBounds = gpkgBBoxForSWNE(...mapTiles.tileToBBoxMeters([0, 0, 0]));
-const bboxMeters = mapTiles.bboxToMeters(bbox);
-const contentsBounds = gpkgBBoxForSWNE(...bboxMeters);
-const tileSize = 256 * scale;
-const metaTileSize = 8 * tileSize;
-
-mapnik.register_default_fonts();
-mapnik.register_default_input_plugins();
-
-const allMetaTiles = function* () {
-  let zoom = maxZoom + 1;
-  while (zoom-- > minZoom) {
-    yield* MetaTile.metaTilesIntersectingBBox(...bbox, zoom);
-  }
+const argsLoader = function(postProcessors) {
+  const emitter = new (require('events').EventEmitter)();
+  return function(confabulous, cb) {
+    setImmediate(function() {
+      const args = commandLineArgs(commandLineOptions, { camelCase: true });
+      const async = require('async');
+      async.seq.apply(async, postProcessors)(args, cb);
+    });
+    return emitter;
+  };
 };
 
-const cutXYZTiles = function(metaTile, metaImage, geoPackage) {
-  const tileDao = geoPackage.getTileDao(tableName);
-  for (let tile of metaTile.xyzTiles()) {
-    const [x, y] = tile;
-    const px = (x - metaTile.x) * tileSize;
-    const py = (y - metaTile.y) * tileSize;
-    metaImage.view(px, py, tileSize, tileSize).encode('png', function(err, buffer) {
-      if (err) {
-        throw err;
-      }
-      console.log('adding tile ' + [x, y, metaTile.zoom]);
-      if (tileDao.queryForTile(x, y, metaTile.zoom)) {
-        console.log('table ' + tableName + ' already contains tile ' + [x, y, metaTile.zoom]);
-      }
-      else {
-        geoPackage.addTile(buffer, tableName, metaTile.zoom, y, x);
-      }
-    });
+const validateConfig = function(config) {
+  const errors = [];
+  if (!config.bbox) {
+    errors.push('missing bbox');
   }
+  if (!config.zoomMin) {
+    errors.push('missing min zoom');
+  }
+  if (!config.zoomMax) {
+    errors.push('missing max zoom');
+  }
+  return errors;
 };
 
-const addTileMatrixSetInGeoPackage = function(gpkg) {
-  if (gpkg.hasTileTable(tableName)) {
-    const missingZoomLevels = {};
-    for (let zoom = minZoom; zoom <= maxZoom; zoom++) {
-      missingZoomLevels[zoom] = null;
-    }
-    const tileDao = gpkg.getTileDao(tableName);
-    tileDao.tileMatrices.forEach(matrix => {
-      if (matrix.zoom_level in missingZoomLevels) {
-        delete missingZoomLevels[matrix.zoom_level];
+const configure = new Confabulous()
+  .add(config => Confabulous.loaders.env([
+    Confabulous.processors.envToCamelCaseProp({ prefix: 'WETSAW_', filter: /^WETSAW_/ }),
+    function(config, callback) {
+      if (config.xsltParam) {
+        if (config.xsltParam.startsWith('[')) {
+          config.xsltParam = JSON.parse(config.xsltParam);
+        }
+        else {
+          config.xsltParam = [config.xsltParam];
+        }
       }
-    });
-    for (let zoom in missingZoomLevels) {
-      gpkg.createStandardWebMercatorTileMatrix(matrixSetBounds, tileDao.tileMatrixSet, zoom, zoom, tileSize);
+      callback(null, config);
     }
-    return Promise.resolve(gpkg);
-  }
-  return gpkgUtil.createStandardWebMercatorTileTable(gpkg, tableName, contentsBounds, 3857, matrixSetBounds, 3857, minZoom, maxZoom, tileSize)
-    .catch(err => {
+  ]))
+  .add(config => argsLoader())
+  .end((err, config) => {
+    if (err) {
       throw err;
-    })
-    .then(_ => gpkg);
-};
+    }
+    const paramsUsage = { header: 'Parameters:', optionList: commandLineOptions };
+    const validationErrors = validateConfig(config);
 
-const setContentsAttrs = function(gpkg) {
-  if (!tableLabel && !tableDesc) {
-    return gpkg;
-  }
-  const contentsDao = gpkg.getContentsDao();
-  const contents = contentsDao.queryForId(tableName);
-  if (tableLabel) {
-    contents.identifier = tableLabel;
-  }
-  if (tableDesc) {
-    contents.description = tableDesc;
-  }
-  const result = contentsDao.update(contents);
-  if (result.changes != 1) {
-    console.log('warning: failed to set contents label/description; ' + result.changes + ' contents rows affected');
-  }
-  return gpkg;
-};
-
-const applyMapnikStyleTransform = () => {
-  if (!xsltPath) {
-    return Promise.resolve(fs.readFileSync(stylePath, 'utf-8'));
-  }
-  return new Promise((resolve, reject) => {
-    console.log('transforming mapnik style with stylesheet ' + xsltPath + '...');
-    const transform = xsltproc.transform(xsltPath, stylePath, { stringparam: xsltParamList });
-    transform.stdout.on('data', function(data) {
-      resolve(data.toString('utf-8'));
-    });
-    transform.stderr.on('data', function(data) {
-      reject(data.toString('utf-8'));
-    });
+    if (config.help) {
+      console.log(commandLineUsage([paramsUsage]));
+      process.exit(0);
+    }
+    if (validationErrors.length > 0) {
+      console.log(commandLineUsage([
+        {
+          header: 'Invalid parameters:',
+          content: validationErrors.join("\n")
+        },
+        paramsUsage
+      ]));
+      process.exit(1);
+    }
+    wetsaw(config).then(
+      _ => console.log('wetsaw complete'),
+      err => { throw err; });
   });
-};
 
-const buildMapPool = () => {
-  return applyMapnikStyleTransform().then(
-    xformResult => {
-      const pool = mapnikPool.fromString(xformResult, { size: metaTileSize, bufferSize: 0 }, { base: styleDir });
-      pool.acquireMap = function() {
-        return new Promise(function(resolve, reject) {
-          pool.acquire(function(err, map) {
-            if (err) {
-              reject(err);
-            }
-            else {
-              resolve(map);
-            }
-          });
-        });
-      }.bind(pool);
-      return pool;
-    },
-    err => {
-      throw err;
-    });
-};
-
-mkdirp.sync(gpkgDir);
-
-buildMapPool().then(mapPool => {
-  return gpkgUtil.create(gpkgPath)
-    .then(gpkg => gpkg, err => {
-      console.log('error opening geopackage: ' + err);
-      throw err;
-    })
-    .then(gpkg => {
-      return addTileMatrixSetInGeoPackage(gpkg);
-    })
-    .then(gpkg => {
-      return setContentsAttrs(gpkg);
-    })
-    .then(gpkg => {
-      for (let metaTile of allMetaTiles()) {
-        mapPool.acquireMap()
-        .then(function(map) {
-          map.zoomToBox(metaTile.bboxMeters());
-          const im = new mapnik.Image(metaTileSize, metaTileSize);
-          return new Promise((resolve, reject) => {
-            map.render(im, {scale: scale, variables: {zoom: metaTile.zoom}}, (err, im) => {
-              mapPool.release(map);
-              if (err) {
-                reject(err);
-              }
-              else {
-                resolve(im);
-              }
-            });
-          });
-        })
-        .catch(err => {
-          throw err;
-        })
-        .then(mapnikImage => {
-          return cutXYZTiles(metaTile, mapnikImage, gpkg);
-        });
-      }
-    });
-}, err => {
-  throw err;
-});
---
+++ wetsaw.js
@@ -0,0 +1,250 @@
+
+/**
+ * @module wetsaw
+ */
+
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+const MetaTile = require('./meta-tile');
+
+const mapnik = require('mapnik');
+const mapnikPool = require('mapnik-pool')(mapnik);
+const mapTiles = require('global-mercator');
+const gpkgUtil = require('@ngageoint/geopackage');
+const untildify = require('untildify');
+const xsltproc = require('xsltproc');
+
+/**
+ * Create a GeoPackage `BoundingBox` object from the given coordinates.
+ * @param {number} west
+ * @param {number} south
+ * @param {number} east
+ * @param {number} north
+ */
+function gpkgBBoxForSWNE(west, south, east, north) {
+  return new gpkgUtil.BoundingBox(west, east, south, north);
+}
+
+ /**
+  * @class WetsawTask
+  */
+class Task {
+
+  /**
+   *
+   * @param {module:wetsaw~WetsawConfig} config
+   */
+  constructor(config) {
+    this.config = config;
+    this.bbox = config.bbox;
+    this.zoomMin = config.zoomMin;
+    this.zoomMax = config.zoomMax;
+    this.stylePath = path.resolve(untildify(config.style));
+    const styleName = path.basename(this.stylePath).replace(/\.[^.]+$/, '').replace(/\W/g, '_').replace(/_{2,}/g, '_');
+    this.styleDir = path.dirname(this.stylePath);
+    this.xsltPath = config.xslt ? path.resolve(untildify(config.xslt)) : null;
+    this.xsltParams = config.xsltParam;
+    this.gpkgPath = path.resolve(untildify(config.gpkg || styleName + '.gpkg'));
+    if (this.gpkgPath.slice(-5) != '.gpkg') {
+      this.gpkgPath += '.gpkg';
+    }
+    this.gpkgDir = path.dirname(this.gpkgPath);
+    this.tableName = config.table || styleName;
+    this.matrixSetBounds = gpkgBBoxForSWNE(...mapTiles.tileToBBoxMeters([0, 0, 0]));
+    const bboxMeters = mapTiles.bboxToMeters(config.bbox);
+    this.contentsBounds = gpkgBBoxForSWNE(...bboxMeters);
+    this.scale = config.scale;
+    this.tileSize = 256 * this.scale;
+    this.metaTileSize = 8 * this.tileSize;
+    this.pool = null;
+  }
+
+  applyMapnikStyleTransform() {
+    if (!this.xsltPath) {
+      return Promise.resolve(fs.readFileSync(this.stylePath, 'utf-8'));
+    }
+    return new Promise((resolve, reject) => {
+      console.log('transforming mapnik style with stylesheet ' + this.xsltPath + '...');
+      const transform = xsltproc.transform(this.xsltPath, this.stylePath, { stringparam: this.xsltParams });
+      let result = Buffer.alloc(0);
+      transform.stdout.on('data', function(data) {
+        result = Buffer.concat([result, data]);
+      });
+      transform.stderr.on('data', function(data) {
+        result = Buffer.concat([result, data]);
+      });
+      transform.on('exit', (code, signal) => {
+        if (code === 0) {
+          resolve(result.toString('utf-8'));
+        }
+        else {
+          reject(result.toString('utf-8'));
+        }
+      });
+    });
+  }
+
+  buildMapnikPool() {
+    return this.applyMapnikStyleTransform().then(
+      xformResult => {
+        this.pool = mapnikPool.fromString(xformResult, { size: this.metaTileSize, bufferSize: 0 }, { base: this.styleDir });
+        this.pool.acquireMap = function() {
+          return new Promise((resolve, reject) => {
+            this.acquire((err, map) => {
+              if (err) {
+                reject(err);
+              }
+              else {
+                resolve(map);
+              }
+            });
+          });
+        }.bind(this.pool);
+        return this.pool;
+      },
+      err => { throw err; });
+  }
+
+  addTileMatrixSetInGeoPackage() {
+    if (this.gpkg.hasTileTable(this.tableName)) {
+      const missingZoomLevels = {};
+      for (let zoom = this.zoomMin; zoom <= this.zoomMax; zoom++) {
+        missingZoomLevels[zoom] = null;
+      }
+      const tileDao = this.gpkg.getTileDao(this.tableName);
+      tileDao.tileMatrices.forEach(matrix => {
+        if (matrix.zoom_level in missingZoomLevels) {
+          delete missingZoomLevels[matrix.zoom_level];
+        }
+      });
+      for (let zoom in missingZoomLevels) {
+        this.gpkg.createStandardWebMercatorTileMatrix(matrixSetBounds, tileDao.tileMatrixSet, zoom, zoom, this.tileSize);
+      }
+      return Promise.resolve(this.gpkg);
+    }
+    return gpkgUtil.createStandardWebMercatorTileTable(
+      this.gpkg, this.tableName, this.contentsBounds, 3857,
+      this.matrixSetBounds, 3857, this.zoomMin, this.zoomMax, this.tileSize)
+      .then(_ => this.gpkg, err => { throw err });
+  }
+
+  setContentsAttrs() {
+    if (!this.tableLabel && !this.tableDesc) {
+      return this.gpkg;
+    }
+    const contentsDao = this.gpkg.getContentsDao();
+    const contents = contentsDao.queryForId(this.tableName);
+    if (this.tableLabel) {
+      contents.identifier = this.tableLabel;
+    }
+    if (this.tableDesc) {
+      contents.description = this.tableDesc;
+    }
+    const result = contentsDao.update(contents);
+    if (result.changes != 1) {
+      console.log('warning: failed to set contents label/description; ' + result.changes + ' contents rows affected');
+    }
+    return this.gpkg;
+  }
+
+  prepareGeoPackage() {
+    return gpkgUtil.create(this.gpkgPath)
+      .then(gpkg => this.gpkg = gpkg, err => {
+        console.log('error opening geopackage: ' + err);
+        throw err;
+      })
+      .then(_ => this.addTileMatrixSetInGeoPackage())
+      .then(_ => this.setContentsAttrs());
+  }
+
+  cutXYZTiles(metaTile, metaImage) {
+    const tileDao = this.gpkg.getTileDao(this.tableName);
+    for (let tile of metaTile.xyzTiles()) {
+      const [x, y] = tile;
+      const px = (x - metaTile.x) * this.tileSize;
+      const py = (y - metaTile.y) * this.tileSize;
+      metaImage.view(px, py, this.tileSize, this.tileSize).encode('png', (err, buffer) => {
+        if (err) {
+          throw err;
+        }
+        console.log('adding tile ' + [x, y, metaTile.zoom]);
+        if (tileDao.queryForTile(x, y, metaTile.zoom)) {
+          console.log('table ' + this.tableName + ' already contains tile ' + [x, y, metaTile.zoom]);
+        }
+        else {
+          this.gpkg.addTile(buffer, this.tableName, metaTile.zoom, y, x);
+        }
+      });
+    }
+  }
+
+  processMetaTile(metaTile) {
+    return this.pool.acquireMap()
+      .then(map => {
+        map.zoomToBox(metaTile.bboxMeters());
+        const im = new mapnik.Image(this.metaTileSize, this.metaTileSize);
+        return new Promise((resolve, reject) => {
+          map.render(im, {scale: this.scale, variables: {zoom: metaTile.zoom}}, (err, im) => {
+            this.pool.release(map);
+            if (err) {
+              reject(err);
+            }
+            else {
+              resolve(im);
+            }
+          });
+        })
+      })
+      .then(mapnikImage => this.cutXYZTiles(metaTile, mapnikImage))
+  }
+
+  *allMetaTiles() {
+    let zoom = this.zoomMax + 1;
+    while (zoom-- > this.zoomMin) {
+      yield* MetaTile.metaTilesIntersectingBBox(...this.bbox, zoom);
+    }
+  }
+
+  generateTiles() {
+    const metaTilePromises = function * () {
+      const metaTiles = this.allMetaTiles();
+      for (let metaTile of metaTiles) {
+        yield this.processMetaTile(metaTile);
+      }
+    }.bind(this);
+    return Promise.all(metaTilePromises());
+  }
+}
+
+/**
+ * Generate the tiles and GeoPackage for the given configuration.
+ *
+ * @param {module:wetsaw~WetsawConfig}
+ */
+module.exports = function(config) {
+  mapnik.register_default_fonts();
+  mapnik.register_default_input_plugins();
+  const task = new Task(config);
+  mkdirp.sync(task.gpkgDir);
+  return task
+    .buildMapnikPool()
+    .then(_ => task.prepareGeoPackage())
+    .then(_ => task.generateTiles());
+};
+
+/**
+ * @typedef {object} WetsawConfig
+ * @property {number[]} bbox `[west, south, east, north]`
+ * @property {number} zoomMin minimum zoom level
+ * @property {number} zoomMax maximum zoom level
+ * @property {string} style path to the Mapnik XML style
+ * @property {?string} gpkg path to the output GeoPackage
+ * @property {?string} table name of the tile table to populate in the GeoPackage
+ * @property {?string} tableLabel human-readable label for the tile table
+ * @property {?string} tableDesc human-readable description of the tile table
+ * @property {?number} scale tile image scale
+ * @property {string} xslt path to an XSLT stylesheet to transform the Mapnik style
+ * @property {string[]} xsltParam parameters to the XSLT stylesheet in the format <name>:<value>
+ */
